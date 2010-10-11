f1cab400d72b88fafa1e04174232fbcdf56203ff
Arturas Slajus <x11@arturaz.net>
2010-10-11 13:03:31 +0300

    create_galaxy in ControlManager

--- server/lib/server/control_manager.rb
+++ server/lib/server/control_manager.rb
@@ -2,6 +2,16 @@
 class ControlManager
   include Singleton
 
+  # Create a new galaxy.
+  #
+  # Parameters:
+  # - ruleset (String): ruleset for given galaxy
+  #
+  # Response:
+  # - galaxy_id (Fixnum): ID of created galaxy
+  #
+  ACTION_CREATE_GALAXY = 'create_galaxy'
+
   # Create a new player in galaxy.
   # 
   # Parameters:
@@ -25,9 +35,19 @@ class ControlManager
   private
   def process(io, message)
     case message['action']
+    when ACTION_CREATE_GALAXY
+      action_create_galaxy(io, message)
     when ACTION_CREATE_PLAYER
       action_create_player(io, message)
     end
+  end
+
+  def action_create_galaxy(io, message)
+    galaxy = Galaxy.new
+    galaxy.ruleset = message['ruleset']
+    galaxy.save!
+
+    io.send_message :galaxy_id => galaxy.id
   end
 
   def action_create_player(io, message)
