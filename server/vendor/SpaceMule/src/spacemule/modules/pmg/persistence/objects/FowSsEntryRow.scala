ae4be00b50907a6f99c6e4825a5e8858a572b215
Arturas Slajus <x11@arturaz.net>
2010-11-03 14:00:51 +0200

    Fixed spacemule.pmg ss entry generation.

--- server/vendor/SpaceMule/src/spacemule/modules/pmg/persistence/objects/FowSsEntryRow.scala
+++ server/vendor/SpaceMule/src/spacemule/modules/pmg/persistence/objects/FowSsEntryRow.scala
@@ -11,17 +11,11 @@ import spacemule.persistence.DB
 object FowSsEntryRow {
   val columns = "`id`, `solar_system_id`, `player_id`, `alliance_id`, " +
     "`counter`, `player_planets`, `enemy_planets`, `player_ships`"
-
-  def apply(ssRow: SolarSystemRow, playerRow: PlayerRow, counter: Int) =
-    new FowSsEntryRow(ssRow, playerRow, counter)
 }
 
 case class FowSsEntryRow(ssRow: SolarSystemRow, playerId: Option[Int],
                          allianceId: Option[Int], counter: Int=1,
-                         enemy: Boolean=false) {
-  def this(ssRow: SolarSystemRow, playerRow: PlayerRow, counter: Int) = this(
-    ssRow, Some(playerRow.id), None, counter)
-
+                         empty: Boolean=true, enemy: Boolean=false) {
   val values = "%d\t%d\t%s\t%s\t%d\t%d\t%d\t0".format(
     TableIds.fowSsEntries.next,
     ssRow.id,
@@ -34,7 +28,7 @@ case class FowSsEntryRow(ssRow: SolarSystemRow, playerId: Option[Int],
       case None => DB.loadInFileNull
     },
     counter,
-    if (enemy) 0 else 1,
-    if (enemy) 1 else 0
+    if (empty) 0 else { if (enemy) 0 else 1 },
+    if (empty) 0 else { if (enemy) 1 else 0 }
   )
 }
