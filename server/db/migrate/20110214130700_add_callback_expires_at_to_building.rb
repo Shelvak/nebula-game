647adc62c1853d2d56af298df910ce66c1686f01
Arturas Slajus <x11@arturaz.net>
2011-02-15 16:59:10 +0200

    Revert "Merge remote branch 'origin/server' into mikism"
    This reverts commit 8c4d259eb611cd1a29ae67cfaad3894876d3e961, reversing
    changes made to 0eee0d4ff39d422097a94c021a14cb5b13c14f6b.
    
    Conflicts:
    
    	server/db/snapshots/main.sql

--- server/db/migrate/20110214130700_add_callback_expires_at_to_building.rb
+++
@@ -1,9 +0,0 @@
-class AddCallbackExpiresAtToBuilding < ActiveRecord::Migration
-  def self.up
-    add_column :buildings, :cooldown_ends_at, :datetime
-  end
-
-  def self.down
-    raise IrreversibleMigration
-  end
-end
