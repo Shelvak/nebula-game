f51dfa58bd8a9478f18d10775996479b364704b7
Arturas Slajus <x11@arturaz.net>
2011-02-15 16:49:57 +0200

    Removed unnecessary indexes.

--- server/db/migrate/20110215161039_remove_unnecessary_index_from_callbacks.rb
+++ server/db/migrate/20110215161039_remove_unnecessary_index_from_callbacks.rb
@@ -1,6 +1,8 @@
 class RemoveUnnecessaryIndexFromCallbacks < ActiveRecord::Migration
   def self.up
-
+    # These two are duplicates of other indexes.
+    remove_index :callbacks, :name => "time"
+    remove_index :callbacks, :name => "removal"
   end
 
   def self.down
