6c382ada45ae8d52c2543fe5538429d9478f5970
Arturas Slajus <x11@arturaz.net>
2011-02-22 10:52:58 +0200

    Forgot to start quests on player create.

--- server/vendor/SpaceMule/src/spacemule/modules/pmg/persistence/objects/SaveResult.scala
+++ server/vendor/SpaceMule/src/spacemule/modules/pmg/persistence/objects/SaveResult.scala
@@ -1,11 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package spacemule.modules.pmg.persistence.objects
-
-import scala.collection.Set
 
 case class SaveResult(val updatedPlayerIds: Set[Int],
                       val updatedAllianceIds: Set[Int])
