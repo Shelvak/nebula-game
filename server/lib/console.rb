a607836ca6c5a3cde26d9ee1d0c4e8334f9ddf8d
Arturas Slajus <x11@arturaz.net>
2011-03-09 18:38:48 +0200

    Require consolerc when launching console.

--- server/lib/console.rb
+++ server/lib/console.rb
@@ -1,7 +1,9 @@
 #!/usr/bin/env ruby
 initializer = File.expand_path(
   File.join(File.dirname(__FILE__), 'initializer.rb'))
-libs = "-r irb/completion -r '#{initializer}'"
+consolerc = File.expand_path(
+  File.join(File.dirname(__FILE__), 'consolerc.rb'))
+libs = "-r irb/completion -r '#{initializer}' -r '#{consolerc}'"
 
 irb = RUBY_PLATFORM =~ /(:?mswin|mingw)/ ? 'irb.bat' : 'irb'
 puts "Starting irb..."
