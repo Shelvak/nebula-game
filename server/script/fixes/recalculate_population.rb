09a4e9ac88410686d3a55072a8457c870da94dc7
Arturas Slajus <x11@arturaz.net>
2011-11-09 19:43:45 +0200

    Don't calculate inactive poplation buildings  when recalculating population.

--- server/script/fixes/recalculate_population.rb
+++ server/script/fixes/recalculate_population.rb
@@ -9,7 +9,7 @@ Player.all.each do |player|
   player.population = player.units.map(&:population).sum
   player.population_cap = SsObject::Planet.where(:player_id => player.id).
     all.map do |planet|
-      planet.buildings.each.map(&:population)
+      planet.buildings.accept(&:active?).map(&:population)
     end.flatten.sum + Cfg.player_initial_population
   player.save!
   $stdout.write "."
