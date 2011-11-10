1c5c65abb9371f1340d68c6c55809b4d127f64c5
Arturas Slajus <x11@arturaz.net>
2011-11-10 17:20:39 +0200

    Fix recalculate_population script.

--- server/script/fixes/recalculate_population.rb
+++ server/script/fixes/recalculate_population.rb
@@ -5,14 +5,17 @@
 STDOUT.sync = true
 require File.dirname(__FILE__) + '/../../lib/initializer.rb'
 
-Player.all.each do |player|
+puts
+players = Player.all
+players.each_with_index do |player, index|
+  $stdout.write "\r#{index + 1}/#{players.size}"
+
   player.population = player.units.map(&:population).sum
   player.population_cap = SsObject::Planet.where(:player_id => player.id).
     all.map do |planet|
-      planet.buildings.accept(&:active?).map(&:population)
+      planet.buildings.reject(&:inactive?).map(&:population)
     end.flatten.sum + Cfg.player_initial_population
   player.save!
-  $stdout.write "."
 end
 
 puts
