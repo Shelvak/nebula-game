a9e7d607d85d9869582ddc806a282ffc6b968fc5
Arturas Slajus <x11@arturaz.net>
2011-11-17 14:11:58 +0200

    You can now target specific users with population recalculator.

--- server/script/fixes/recalculate_population.rb
+++ server/script/fixes/recalculate_population.rb
@@ -6,9 +6,10 @@ STDOUT.sync = true
 require File.dirname(__FILE__) + '/../../lib/initializer.rb'
 
 puts
-players = Player.all
+players = ARGV.empty? ? Player.all : Player.where(:name => ARGV).all
 players.each_with_index do |player, index|
-  $stdout.write "\r#{index + 1}/#{players.size}"
+  $stdout.write "\r#{player.name} @ galaxy id #{player.galaxy_id} (#{
+    index + 1}/#{players.size})"
 
   player.population = player.units.map(&:population).sum
   player.population_cap = SsObject::Planet.where(:player_id => player.id).
