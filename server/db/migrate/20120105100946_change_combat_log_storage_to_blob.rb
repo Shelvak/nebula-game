e3e91e2fbe874272b5a5a6ded36533aa6683cf60
Arturas Slajus <x11@arturaz.net>
2012-01-05 13:22:19 +0200

    Change storage back.

--- server/db/migrate/20120105100946_change_combat_log_storage_to_blob.rb
+++ server/db/migrate/20120105100946_change_combat_log_storage_to_blob.rb
@@ -1,18 +1,21 @@
 class ChangeCombatLogStorageToBlob < ActiveRecord::Migration
   def self.up
-    execute "TRUNCATE `combat_logs`"
     change_column :combat_logs, :info, 'LONGBLOB', :null => false
     execute "ALTER TABLE `combat_logs` DROP PRIMARY KEY"
     add_index :combat_logs, :sha1_id, :unique => true, :name => :lookup
-    remove_column :combat_logs, :expires_at
     add_column :combat_logs, :id, :primary_key
+
+    log = CombatLog.new
+    CombatLog.select("id, expires_at").c_select_all.each do |row|
+      log.id = row['id']
+      time = Time.parse(row['expires_at'])
+      CallbackManager.register(log, CallbackManager::EVENT_DESTROY, time)
+    end
+
+    remove_column :combat_logs, :expires_at
   end
 
   def self.down
-    execute "TRUNCATE `combat_logs`"
-    change_column :combat_logs, :info, 'TEXT', :null => false
-    remove_column :combat_logs, :id
-    add_column :combat_logs, :expires_at, :datetime, :null => false
-    execute "ALTER TABLE combat_logs` ADD PRIMARY KEY (`sha1_id`)"
+    raise IrreversibleMigration
   end
 end
