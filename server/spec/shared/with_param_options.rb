f9b9c43d9af4ddeae480865cb5e12bcc018f71b7
Arturas Slajus <x11@arturaz.net>
2012-01-19 14:45:31 +0200

    Convert arguments to strings in "with param options".

--- server/spec/shared/with_param_options.rb
+++ server/spec/shared/with_param_options.rb
@@ -2,7 +2,7 @@ shared_examples_for "with param options" do |required_params|
   required_params.each do |param|
     it "should require #{param} parameter" do
       lambda do
-        @params.delete param
+        @params.delete param.to_s
         send(@method || 'invoke', @action, @params)
       end.should raise_error(ControllerArgumentError)
     end
