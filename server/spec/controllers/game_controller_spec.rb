254d490dfffa6c5feb361762459ff25be9d42de5
Arturas Slajus <x11@arturaz.net>
2012-01-21 16:29:14 +0200

    Allow game|config to be requested without being logged in.

--- server/spec/controllers/game_controller_spec.rb
+++ server/spec/controllers/game_controller_spec.rb
@@ -3,39 +3,65 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper.rb
 describe GameController do
   include ControllerSpecHelper
 
-  before(:each) do
-    init_controller GameController, :login => true
-  end
-
-  describe "game|config" do
+  describe "not logged in" do
     before(:each) do
-      @action = "game|config"
-      @params = {}
+      init_controller GameController, :login => false
     end
 
-    it "should have config filtered by regexp" do
-      invoke @action
-      response[:config].should equal_to_hash(
-        CONFIG.constantize_speed(CONFIG.filter(GameController::SENDABLE_RE))
-      )
-    end
+    describe "game|config" do
+      before(:each) do
+        @action = "game|config"
+        @params = {}
+      end
 
-    it "should replace speed with constant" do
-      with_config_values 'units.foo' => '10 / speed * level' do
-        invoke @action, @params
-        response[:config]['units.foo'].should == "10 / #{
-          CONFIG['speed']} * level"
+      it "should have config filtered by regexp (default ruleset)" do
+        invoke @action
+        response[:config].should equal_to_hash(
+          CONFIG.constantize_speed(
+            CONFIG.filter(GameController::SENDABLE_RE, 'default')
+          )
+        )
       end
     end
+  end
 
-    it "should recognize different rulesets" do
-      with_config_values 'units.foo' => '10 / speed' do
-        invoke @action, @params
-        default_value = response[:config]['units.foo']
+  describe "logged in" do
+    before(:each) do
+      init_controller GameController, :login => true
+    end
 
-        session[:ruleset] = 'hyper'
-        invoke @action, @params
-        response[:config]['units.foo'].should_not == default_value
+    describe "game|config" do
+      before(:each) do
+        @action = "game|config"
+        @params = {}
+      end
+
+      it "should have config filtered by regexp" do
+        invoke @action
+        response[:config].should equal_to_hash(
+          CONFIG.constantize_speed(
+            CONFIG.filter(GameController::SENDABLE_RE, session[:ruleset])
+          )
+        )
+      end
+
+      it "should replace speed with constant" do
+        with_config_values 'units.foo' => '10 / speed * level' do
+          invoke @action, @params
+          response[:config]['units.foo'].should == "10 / #{
+            CONFIG['speed']} * level"
+        end
+      end
+
+      it "should recognize different rulesets" do
+        with_config_values 'units.foo' => '10 / speed' do
+          invoke @action, @params
+          default_value = response[:config]['units.foo']
+
+          session[:ruleset] = 'hyper'
+          invoke @action, @params
+          response[:config]['units.foo'].should_not == default_value
+        end
       end
     end
   end
