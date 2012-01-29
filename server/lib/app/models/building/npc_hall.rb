95b651c10e3f63cd6686e87c216d838b9de9fbd7
Arturas Slajus <x11@arturaz.net>
2012-01-29 16:36:42 +0200

    Building::ResourceTransporter

--- server/lib/app/models/building/npc_hall.rb
+++ server/lib/app/models/building/npc_hall.rb
@@ -1,7 +1,20 @@
 class Building::NpcHall < Building
-  include Parts::WithCooldown
+  include Parts::WithCallbackableCooldown
   include Parts::ResetableCooldown
   include Parts::LoopedCooldown
+
+  # When the next cooldown should end.
+  def cooldowns_at
+    time_owned = planet.owner_changed.nil? \
+      ? 0 : (Time.now - planet.owner_changed).to_i
+    self.class.cooldowns_at(level, time_owned)
+  end
+
+  # When the next cooldown should end.
+  def self.cooldowns_at(level, time_owned)
+    evalproperty('cooldown', nil,
+      'level' => level, 'time_owned' => time_owned).seconds.from_now
+  end
 
   def cooldown_expired!
     player = planet.player
--- server/lib/app/models/building/resource_transporter.rb
+++ server/lib/app/models/building/resource_transporter.rb
@@ -1,3 +1,79 @@
 class Building::ResourceTransporter < Building
+  include Parts::WithCooldown
 
+  # Raised if target planet does not have a transporter.
+  class NoTransporterError < GameError; end
+
+  def max_volume(level=nil)
+    self.class.max_volume(level || self.level)
+  end
+
+  def self.max_volume(level)
+    evalproperty("max_volume", nil, {'level' => level}).round
+  end
+
+  def cooldown_time(volume, level=nil)
+    self.class.cooldown_time(volume, level || self.level)
+  end
+
+  def self.cooldown_time(volume, level)
+    mod = cooldown_mod(level)
+    volume * mod
+  end
+
+  def self.cooldown_mod(level)
+    evalproperty("mod.cooldown", nil, {'level' => level}).round
+  end
+
+  def fee(level=nil)
+    self.class.fee(level || self.level)
+  end
+
+  def self.fee(level)
+    evalproperty("fee", nil, {'level' => level})
+  end
+
+  def transport!(target_planet, metal, energy, zetium)
+    raise GameLogicError.new(
+      "Building must be active and without cooldown to transport resources!"
+    ) unless active? && cooldown_expired?
+
+    volume = Resources.total_volume(metal, energy, zetium)
+    max_volume = self.max_volume
+    raise GameLogicError.new(
+      "Cannot transport #{metal} m, #{energy} e, #{zetium} z, total volume (#{
+      volume}) is greater than max (#{max_volume})"
+    ) if volume > max_volume
+
+    planet = self.planet
+    raise GameLogicError.new(
+      "Not enough resources to transport! M: req:#{metal}/has:#{
+        planet.metal}, E: req:#{energy}/has:#{planet.energy}, Z: req:#{
+        zetium}/has:#{planet.zetium}"
+    ) unless planet.metal >= metal && planet.energy >= energy &&
+      planet.zetium >= zetium
+
+    raise GameLogicError.new(
+      "Can only transfer to planet which belongs to same player! Source: #{
+      planet}, target: #{target_planet}"
+    ) if planet.player_id != target_planet.player_id
+
+    raise NoTransporterError.new(
+      "#{target_planet} does not have an active resource transporter!"
+    ) unless self.class.where(
+      :planet_id => target_planet.id, :state => STATE_ACTIVE
+    ).exists?
+
+    self.cooldown_ends_at = cooldown_time(volume).seconds.from_now
+    arrival_mult = 1.0 - fee
+
+    save!
+    planet.increase!(:metal => -metal, :energy => -energy, :zetium => -zetium)
+    target_planet.increase!(
+      :metal => metal * arrival_mult, :energy => energy * arrival_mult,
+      :zetium => zetium * arrival_mult
+    )
+
+    EventBroker.fire(self, EventBroker::CHANGED)
+  end
 end
