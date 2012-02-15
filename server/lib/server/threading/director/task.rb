e5d5e66de58756ac635aee400f61e97b4c88971c
Arturas Slajus <x11@arturaz.net>
2012-02-15 21:43:54 +0200

    Trying to fix connection issues... Not working.

--- server/lib/server/threading/director/task.rb
+++ server/lib/server/threading/director/task.rb
@@ -4,9 +4,12 @@ class Threading::Director::Task
     @block = block
   end
 
-  # Runs this task passing it worker name.
+  # Runs this task passing it worker name. Checks out DB connection before
+  # running it.
   def run(worker_name)
-    @block.call(worker_name)
+    ActiveRecord::Base.connection_pool.with_connection do
+      @block.call(worker_name)
+    end
   end
 
   def to_s
