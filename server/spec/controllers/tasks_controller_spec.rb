6fc97967a9a490febce545a51654ceea27f55c50
Arturas Slajus <x11@arturaz.net>
2012-04-17 18:02:29 +0300

    Spec for tasks|create_player.

--- server/spec/controllers/tasks_controller_spec.rb
+++ server/spec/controllers/tasks_controller_spec.rb
@@ -58,4 +58,37 @@ describe TasksController do
       invoke @action, @params
     end
   end
+
+  describe "tasks|create_player" do
+    let(:galaxy) { Factory.create(:galaxy) }
+
+    before(:each) do
+      @action = "tasks|create_player"
+      @params.merge!(
+        'galaxy_id' => galaxy.id,
+        'web_user_id' => (Player.maximum(:web_user_id) || 0) + 1,
+        'name' => "P#{Time.now.to_f}"
+      )
+    end
+
+    it_should_behave_like "with param options",
+      :required => %w{galaxy_id web_user_id name},
+      :needs_login => false, :needs_control_token => true
+
+    it "should call Galaxy.create_player" do
+      Galaxy.should_receive(:create_player).with(
+        galaxy.id, @params['web_user_id'], @params['name']
+      ).and_return({})
+      invoke @action, @params
+    end
+
+    it "should return player id" do
+      invoke @action, @params
+      Player.find(response[:player_id]).should_not be_nil
+    end
+
+    it "should work" do
+      invoke @action, @params
+    end
+  end
 end
