971a011f2a0d8d039fd8abb93ca4b06e375074ef
Arturas Slajus <x11@arturaz.net>
2012-04-19 14:18:34 +0300

    Some fixes to counter helper.

--- server/script/fixes/helpers/counter.rb
+++ server/script/fixes/helpers/counter.rb
@@ -1,14 +1,20 @@
 class Counter
-  def initialize(scope, header)
+  def initialize(scope, header, name_func=nil)
     @scope = scope
     @header = header
+    @name_func = name_func || lambda { |obj| obj.id }
   end
 
   def each
     total = @scope.count
+    if total == 0
+      puts "Nothing to do for #{@header}."
+      return
+    end
+
     index = 1
     @scope.find_each do |obj|
-      $stdout.write("\r#{@header} #{obj.id} (#{index}/#{total})")
+      $stdout.write("\r#{@header} #{@name_func[obj]} (#{index}/#{total})")
       yield obj
       $stdout.write(".")
       index += 1
