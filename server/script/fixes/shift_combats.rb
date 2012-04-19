d57bf96dc280ecaed59ee49d5bd1d01419f3bfce
Arturas Slajus <x11@arturaz.net>
2012-04-19 14:18:43 +0300

    Smarter shift_combats fixer.

--- server/script/fixes/shift_combats.rb
+++ server/script/fixes/shift_combats.rb
@@ -1,10 +1,30 @@
 #!/usr/bin/env ruby
 
+if ARGV.size == 0
+  puts "Usage: #{$0} 'duration'"
+  puts
+  puts "Where:"
+  puts "- *duration* is a time string. "
+  puts "  E.g.: '5 minutes' or '1 hour'"
+  puts
+  puts "Arguments were:"
+  puts "#{ARGV.inspect}"
+  exit
+end
+
+duration = ARGV.join(" ")
+unless duration.match(
+  /^(\d)+ (seconds?|minutes?|hours?|days?|weeks?|months?|years?)$/
+)
+  $stderr.write("Cannot parse date sting: #{duration.inspect}\n")
+  exit 1
+end
+
 STDOUT.sync = true
 require File.dirname(__FILE__) + '/../../lib/initializer.rb'
 
-time = 4.days
-time_str = "INTERVAL #{time} SECOND"
+duration = eval(duration.gsub(" ", "."))
+time_str = "INTERVAL #{duration} SECOND"
 
 ActiveRecord::Base.transaction do
   puts "planet raids"
