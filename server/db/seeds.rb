7c61fcd06adb7b1a76e3d33447544f0f16737804
Mykolas Mickus <mikopas@gmail.com>
2012-04-25 12:12:09 +0300

    Galaxy.create_player() requires 4 parameters, not 3.

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -3,7 +3,7 @@ puts QUESTS.sync!
 
 if Galaxy.count == 0
   galaxy_id = Galaxy.create_galaxy(ENV['ruleset'] || 'dev', "localhost")
-  Galaxy.create_player(galaxy_id, 0, "Test Player")
+  Galaxy.create_player(galaxy_id, 0, "Test Player", false)
   puts "Created first galaxy."
 else
   puts "Galaxy already created."
