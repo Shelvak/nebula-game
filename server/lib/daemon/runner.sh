cf3151e3e0d5a7008ca06adf22f422c43be2919b
Artūras Šlajus <arturas@zeus.(none)>
2012-05-27 20:26:33 +0300

    Add more heap for daemon. Also add support for remote jmx connection.

--- server/lib/daemon/runner.sh
+++ server/lib/daemon/runner.sh
@@ -9,7 +9,18 @@ export JRUBY_OPTS="" # Clear JRuby opts
 export JAVA_OPTS=""  # Clear Java opts
 
 opts="--1.9 -J-Dname=$server_name -J-Djruby.jit.max=25000 \
---server -J-XX:+TieredCompilation -J-XX:MaxPermSize=256m"
+--server -J-XX:+TieredCompilation \
+-X+C -J-Xms512M -J-Xmx1024M -J-XX:MaxPermSize=256m \
+-Xreify.classes=true"
+
+jmxpass="$HOME/config/jmx.password"
+if [ -e "$jmxpass" ]; then
+  opts="$opts \
+-J-Dcom.sun.management.jmxremote.port=55100 \
+-J-Dcom.sun.management.jmxremote.password.file=$jmxpass \
+-J-Dcom.sun.management.jmxremote.ssl=false"
+fi
+
 rundir=$(readlink -f "$(dirname $0)/..")
 logdir=$(readlink -f "$rundir/../log")
 logfile="$logdir/daemon.log"
