100698d199639cd0b49a945e7fef504398ff4225
Arturas Slajus <arturas@nebula44.com>
2012-06-20 10:22:38 +0300

    Remove alliance_visibility index from fow galaxy entries.

--- server/db/migrate/20120615165756_fse_begone.rb
+++ server/db/migrate/20120615165756_fse_begone.rb
@@ -3,6 +3,7 @@ class FseBegone < ActiveRecord::Migration
     drop_table :fow_ss_entries
     FowGalaxyEntry.where("alliance_id IS NOT NULL").delete_all
     remove_fk :fow_galaxy_entries, :alliance_id
+    remove_index :fow_galaxy_entries, :name => :alliance_visiblity
     remove_column :fow_galaxy_entries, :alliance_id
     change_column :fow_galaxy_entries, :player_id, :integer, null: false
   end
