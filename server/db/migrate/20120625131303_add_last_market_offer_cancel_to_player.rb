4f0d56ac18bec826e301ea568840cb887f629c3d
Arturas Slajus <arturas@nebula44.com>
2012-06-27 13:19:15 +0300

    Deployed Player#last_market_offer_cancel. Deployment fixes.

--- server/db/migrate/20120625131303_add_last_market_offer_cancel_to_player.rb
+++ server/db/migrate/20120625131303_add_last_market_offer_cancel_to_player.rb
@@ -1,7 +1,7 @@
 class AddLastMarketOfferCancelToPlayer < ActiveRecord::Migration
   def self.up
     add_column :players, :last_market_offer_cancel, :datetime, null: false,
-      default: "0000-00-00 00:00:00"
+      default: "0000-01-01 00:00:00"
   end
 
   def self.down
