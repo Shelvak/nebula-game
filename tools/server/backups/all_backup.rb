242c8a56ac0753a6cdaba127e5cc4a0056db99d1
Arturas Slajus <arturas@nebula44.com>
2012-06-28 16:09:17 +0300

    Fixed link-dest on remote filesystem.

--- tools/server/backups/all_backup.rb
+++ tools/server/backups/all_backup.rb
@@ -37,8 +37,8 @@ run "#{pwd}/fs_backup.rb"
 
 target="#{HOST}:#{RDIR}"
 
-puts "Ensuring filesystem dir exists in remote."
-run(%Q{#{SSH} #{HOST} "mkdir -p #{RDIR}/mysql"})
+puts "Ensuring mysql dir exists in remote."
+run %Q{#{SSH} #{HOST} "mkdir -p #{RDIR}/mysql"}
 puts "Transferring mysql to remote..."
 run %Q{rsync -a --delete -e "#{SSH}" #{BACKUPS}/mysql/ #{target}/mysql/}
 
@@ -51,18 +51,25 @@ backups = Dir["#{FS_DIR}/*"].map do |path|
   {:path => path, :time => time}
 end.sort_by { |b| b[:time] }
 last = backups[-1][:path].sub(/\/$/, '')
+last_basename = File.basename(last)
 
 puts "Ensuring filesystem dir exists in remote."
-run(%Q{#{SSH} #{HOST} "mkdir -p #{RDIR}/filesystem"})
+run %Q{#{SSH} #{HOST} "mkdir -p #{RDIR}/filesystem"}
 puts "Listing remote filesystem backups..."
 last_remote = `#{SSH} #{HOST} "ls -1 #{RDIR}/filesystem"`
-if last_remote.strip == ""
-  last_remote = ""
-else
-  last_remote = %Q{"--link-dest=#{last_remote.split[-1]}"}
-end
+last_remote = last_remote.strip == "" \
+  ? nil : last_remote.split.reverse.find { |r| r != last_basename }
+
 puts "Transfering filesystem to remote..."
-run %Q{rsync -a -e "#{SSH}" #{last_remote} #{last} #{target}/filesystem/}
+run %Q{rsync -a -e "#{SSH}" #{last} #{target}/filesystem/}
+unless last_remote.nil?
+  puts "Hardlinking filesystem to previous backup in remote..."
+  remote = "#{RDIR}/filesystem/#{last_basename}"
+  remote_ul = "#{remote}-unlinked"
+  run %Q{#{SSH} #{HOST} "mv '#{remote}' '#{remote_ul}' && \
+    rsync -a '--link-dest=../#{last_remote}' '#{remote_ul}/' '#{remote}/' && \
+    rm -rf '#{remote_ul}'"}
+end
 
 puts "Removing temp files..."
 %w{stderr stdout}.each do |f|
